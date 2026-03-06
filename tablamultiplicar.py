0bb072dc86e46c880f77cdbb7ca549dfd4978ee8
valerie samper <valerie.samper01@usa.edu.co>
2026-03-06 07:50:19 -0500

    fix branch

---
+++ tablamultiplicar.py
@@ -0,0 +1,8 @@
+for i in range(1, 11):
+    print("Tabla del " + str(i))
+    for j in range(1, 11):
+        resultado = i * j
+        print(f"{i} x {j} = {resultado}")
+     print()
+
+    
--- taller_1128127186.py
+++ taller_1128127186.py
@@ -1,13 +1,32 @@
 edad=17
 if edad >18:
-    print("Eres mayor de edad")
+  print("Eres mayor de edad")
 else:
-    print("Eres menor de edad")
-
+ print("Eres menor de edad")
 for i in range(5):
-    print("hola mundo")
-#BUCLE WHILE
+   print("hola mundo")
 contador=1
 while contador <=5:
-    print("Numero,"+str(contador))
+  print("Numero:" + str(contador))
 contador = contador + 1
+
+for i in range(5):
+    print("El Número:" + str(i))
+
+for i in range(0,12,6):
+     print("El Número:" + str(i))
+
+
+encontrado =False
+numerobuscado=11
+numeros =[1,3,5,7,9]
+for numero in numeros:
+    if numero == numerobuscado:
+        encontrado = True
+        break
+print("Número", numerobuscado, "encontrado:", encontrado)
+
+
+for i in range(3):
+    for j in range(3):
+        print("i="+ str(i) + " j=" + str(j))
